7a5aed88a6766c3b39270efa083634602efd2a5b
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-17 17:41:21 +0100

    add working grid self empty

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,45 +6,42 @@ const buttonElement = document.getElementById("play_button");
 
 buttonElement.addEventListener('click', function () {
     const containerElement = document.querySelector('.container')
-    // si attiva il gioco
-    if (containerElement.childNodes.length = 0) {
-        // devo generare una cella x 100 volte
+    containerElement.innerHTML = ''
 
-        // seleziono il container dalla DOM
-        // perché se usavo getElementByClassName non va ? perché InnerHTML non gli piace 
+    // devo generare una cella x 100 volte
 
-        const numbOfCells = 100
+    // seleziono il container dalla DOM
+    // perché se usavo getElementByClassName non va ? perché InnerHTML non gli piace 
 
-        for (let i = 1; i < numbOfCells + 1; i++) {
-            const cellCreator = `<div class="cell">${i}</div>`;
-            // creo una variabile con dentro una stringa che ha proprio il markUp giusto
-            // ci scrivo giá il numero con TL che é lo stesso di i
-            containerElement.innerHTML += cellCreator
-            // scrivodentro a container il markup
-        }
+    const numbOfCells = 100
 
-
-        // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
-        const cellElementArray = document.querySelectorAll('.cell')
-
-
-        // selezionare tutte le celle cosí crea una sorta di Array con dentro tutte le singole celle
-
-        for (let i = 0; i < cellElementArray.length; i++) {
-            const thisCell = cellElementArray[i];
-            console.log(thisCell);
-            // ----
-            thisCell.addEventListener("click", function () {
-                thisCell.classList.toggle("activeCell")
-            })
-
-            // ho aggiunto ad ogni cella (ciclando dentro l'array di tutte le celle) un event listener che toggla la classe "activeCell"
-
-        }
-    } else {
-        containerElement.innerHTML = ''
+    for (let i = 1; i < numbOfCells + 1; i++) {
+        const cellCreator = `<div class="cell">${i}</div>`;
+        // creo una variabile con dentro una stringa che ha proprio il markUp giusto
+        // ci scrivo giá il numero con TL che é lo stesso di i
+        containerElement.innerHTML += cellCreator
+        // scrivodentro a container il markup
     }
+
+    // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+    const cellElementArray = document.querySelectorAll('.cell')
+
+
+    // selezionare tutte le celle cosí crea una sorta di Array con dentro tutte le singole celle
+
+    for (let i = 0; i < cellElementArray.length; i++) {
+        const thisCell = cellElementArray[i];
+        console.log(thisCell);
+        // ----
+        thisCell.addEventListener("click", function () {
+            thisCell.classList.toggle("activeCell")
+        })
+
+        // ho aggiunto ad ogni cella (ciclando dentro l'array di tutte le celle) un event listener che toggla la classe "activeCell"
+
+    }
+
 
 
 })
