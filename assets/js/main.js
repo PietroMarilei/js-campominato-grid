615d546c90bfcf8bb8d2120c79447f0ce57de6a8
PietroMarilei~ <pietromrl0@gmail.com>
2023-05-05 16:16:50 +0200

    fixed cell alinment

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
-const buttonElement = document.getElementById("play_button");
+const buttonElement = document.getElementById("playButton");
 
 const selectElement = document.getElementById("difficulty");
 
@@ -16,9 +16,6 @@ buttonElement.addEventListener('click', function () {
     containerElement.innerHTML = ''
     // questo affare svuota la tabella prima che succeda altro
 
-    // seleziono il container dalla DOM
-    // perché se usavo getElementByClassName non va ? perché InnerHTML non gli piace 
-
     // -parte bonus
     numbOfCells = Number(selectElement.value);
     console.log(numbOfCells);
@@ -26,10 +23,13 @@ buttonElement.addEventListener('click', function () {
     // devo generare una cella x 100 volte
     for (let i = 1; i < numbOfCells + 1; i++) {
         const cellCreator = `<div class="cell">${i}</div>`;
+
         // creo una variabile con dentro una stringa che ha proprio il markUp giusto
         // ci scrivo giá il numero con TL che é lo stesso di i
         containerElement.innerHTML += cellCreator
         // scrivodentro a container il markup
+
+
     }
 
     // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
@@ -45,9 +45,14 @@ buttonElement.addEventListener('click', function () {
         // ----
         thisCell.addEventListener("click", function () {
             thisCell.classList.toggle("activeCell")
+
+
         })
 
         // ho aggiunto ad ogni cella (ciclando dentro l'array di tutte le celle) un event listener che toggla la classe "activeCell"
+
+        thisCell.style.width = `calc(100% / ${Math.sqrt(numbOfCells)})`
+        // Perché non va questo ?
 
     }
 
